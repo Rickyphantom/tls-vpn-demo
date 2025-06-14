61dd49ff8783824786e8668d2c9b5bd3d74d3240
ujam1101 <90842067+ujam1101@users.noreply.github.com>
2025-06-14 23:37:16 +0900

    Update page.tsx

--- src/app/lab/encryption/page.tsx
+++ src/app/lab/encryption/page.tsx
@@ -9,9 +9,6 @@ export default function EncryptionMenuPage() {
           🔐 <Link href="/lab/encryption/tls-rsa">RSA 실습</Link>
         </li>
         <li>
-          📡 <Link href="/lab/encryption/tls-dh">DH 실습</Link>
-        </li>
-        <li>
           🧬 <Link href="/lab/encryption/tls-ecdhe">ECDHE 실습</Link>
         </li>
         <li>
